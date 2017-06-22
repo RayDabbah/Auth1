cee8537f69a4a1890f877cf4f76caa0aaddb36ca
raydabbah <dabbahray@yahoo.com>
2017-06-21 21:38:51 -0400

    all auth works!!!

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 var express= require("express");
-var mongoose = require("mongoose");
+var mongoose = require("mongoose"),
 bodyParser = require('body-parser'),
 passport = require('passport'),
 LocalStrategy = require('passport-local'),
 passportLocalMongoose = require('passport-local-mongoose');
-User = require("./models/user.js");
+User = require("./models/user");
+
 mongoose.connect("mongodb://localhost/auth");
 var app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(require("express-session")({
-    secret: 'Javascript can be strange!',
+    secret: 'Javascript can be strange! gjkhguyff hhhrss',
     resave: false,
     saveUninitialized: false,
-}))
+}));
+ app.use(express.static("public"));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+// middleware for authenticaton
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+     res.redirect("/login");
+}
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -24,9 +34,13 @@ app.get("/", (req, res) =>{
     res.render("home");
 });
 
-app.get("/secret", (req, res)=> {
+app.get("/secret",isLoggedIn, (req, res)=> {
     res.render("secret");
 });
+
+// =======================
+//         SIGNUP
+// =======================
 app.get("/register", (req, res) => {
     res.render("register");
 });
@@ -45,5 +59,24 @@ app.post("/register", (req, res) => {
     });
    
 });
+// ==================
+//     LOGIN
+// ==================
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+app.post("/login", passport.authenticate('local', {
+    successRedirect: "/secret",
+    failureRedirect: "/login",
+}), (req, res) => {
+    res.send("Hi POST!");
+});
+// ===============
+//     LOG OUT
+// ===============
+app.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+});
 
-app.listen(3000, 'localhost', ()=> console.log('Server is listening...'))
+app.listen(3000, 'localhost', ()=> console.log('Server is listening...'));
